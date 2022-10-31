f55e391c9c15b92f690bb18af1fc0fc32bc641d8
bhaskarkh01 <bhaskarkh01@gmail.com>
2022-11-01 01:54:02 +0530

    add AppBar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,9 +8,23 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late double _deviceHeight, _deviceWidth;
+
   _HomePageState();
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: _deviceHeight * 0.1,
+        title: Text(
+          "Taskly!",
+          style: TextStyle(
+            fontSize: 25,
+          ),
+        ),
+      ),
+    );
   }
 }
