f25edf525a7ed6c508f01ad7cf9a1957e8522373
bhaskarkh01 <bhaskarkh01@gmail.com>
2022-11-01 19:57:57 +0530

    updating and deleting tasks

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -39,7 +39,7 @@ class _HomePageState extends State<HomePage> {
     return FutureBuilder(
       future: Hive.openBox('tasks'),
       builder: (BuildContext _context, AsyncSnapshot _snapshot) {
-        if (_snapshot.connectionState == ConnectionState.done) {
+        if (_snapshot.hasData) {
           _box = _snapshot.data;
           return _tasksList();
         } else {
@@ -76,6 +76,18 @@ class _HomePageState extends State<HomePage> {
                 : Icons.check_box_outline_blank_outlined,
             color: Colors.red,
           ),
+          onTap: () {
+            task.done = !task.done;
+            _box!.putAt(
+              _index,
+              task.toMap(),
+            );
+            setState(() {});
+          },
+          onLongPress: () {
+            _box!.deleteAt(_index);
+            setState(() {});
+          },
         );
       },
     );
@@ -95,7 +107,19 @@ class _HomePageState extends State<HomePage> {
         return AlertDialog(
           title: const Text("Add new task: "),
           content: TextField(
-            onSubmitted: (_value) {},
+            onSubmitted: (_value) {
+              if (_newTaskContent != null) {
+                var _task = Task(
+                    content: _newTaskContent!,
+                    timestamp: DateTime.now(),
+                    done: false);
+                _box!.add(_task.toMap());
+                setState(() {
+                  _newTaskContent = null;
+                  Navigator.pop(context);
+                });
+              }
+            },
             onChanged: (_value) {
               setState(() {
                 _newTaskContent = _value;
